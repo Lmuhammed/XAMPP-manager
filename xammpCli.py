491cfd63b0f9ce67b15c667583ccc6f88898a030
L-mohamed <abdorahman_23@proton.me>
2024-05-19 00:28:02 +0100

    add optional parameter to oneOrMulti_services() to check for all services

--- xammpCli.py
+++ xammpCli.py
@@ -10,7 +10,7 @@ def help_menu():
     script_name = os.path.basename(__file__)
     print(f"Usage: {script_name} [Service] <Action>")
     print("Service  :")
-    print("  -p / --apache       Select Apache")
+    print("  -ap / --apache       Select Apache")
     print("  db / --mysql        Select Database (MariadDB)")
     print("  -ftp                Select ProFTPD")
     print("  all                 Select apache ftp & database")
@@ -19,8 +19,9 @@ def help_menu():
     print("  off                 Turn off the service/services")
     exit(1)
 
-def one_action(service,argument):
-    if service == 'all':
+def oneOrMulti_services(service,argument,allServices=None):
+    #Check if all services 
+    if allServices is True:
         if argument == "on" :
             command="sudo ./xampp startapache ; sudo ./xampp startmysql ; sudo ./xampp startftp"
             os.system(command)
@@ -28,15 +29,19 @@ def one_action(service,argument):
             command="sudo ./xampp stopapache ; sudo ./xampp stopmysql ; sudo ./xampp stopftp"
             os.system(command)
         else:
-            print("error")
+            print(f"Invalide argument with : all ")
+            sys.exit()
+    #Check if a service checked : apache , mysql or ftp            
     else :
         if argument == "on" :
             command="sudo ./xampp start"+service
             os.system(command)
+
         elif argument == "off" :
             command="sudo ./xampp stop"+service
             os.system(command)
         else:
+            print(f"Invalid parameter  name : {argument} ")
             help_menu()
             
 #End Functions Declaration
@@ -52,27 +57,26 @@ param2 = (sys.argv[2]).lower()
 service=None
 
 if param == "all" :
-    service='all'
-    one_action(service,param2)
+    oneOrMulti_services(service,param2,True)
     exit(1)
 
 elif param == "-ap" or param == "--apache":
     service='apache'
-    one_action(service,param2)
+    oneOrMulti_services(service,param2)
     exit(1)
 
 elif param == "-db" or param == "--mysql":
     service='mysql'
-    one_action(service,param2)
+    oneOrMulti_services(service,param2)
     exit(1)
 
 elif param == "-ftp":
     service='ftp'
-    one_action(service,param2)
+    oneOrMulti_services(service,param2)
     exit(1)
 
 else :
-    print("Error ...Retype ")
+    print(f"Invalid Service name : {param} ")
     exit(2)
 
 
